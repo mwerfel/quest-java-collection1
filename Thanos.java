ac8f7b0ef1b929be72280abd9a9ff1e2d8e0973c
Mike <mike-werfel_student2022@wilder.school>
2022-03-09 15:41:33 +0100

    Challenge accepted...

--- Thanos.java
+++ Thanos.java
@@ -4,24 +4,54 @@ public class Thanos {
 
     public static void main(String[] args) {
 
-        // TODO 1 : Create an empty heroes list
 
-        // TODO 2 : Add those heroes to the list
-        // name: Black Widow, age: 34
-        // name: Captain America, age: 100
-        // name: Vision, age: 3
-        // name: Iron Man, age: 48
-        // name: Scarlet Witch, age: 29
-        // name: Thor, age: 1500
-        // name: Hulk, age: 49
-        // name: Doctor Strange, age: 42
+     // Create a new Heroes Array List and add Heroes and Heroins
 
-        // TODO 3 : It's Thor birthday, now he's 1501
+     ArrayList<Hero> heroes = new ArrayList<Hero>();
 
-        // TODO 4 : Shuffle the heroes list
+        heroes.add(new Hero("Black Widow",34));
+        heroes.add(new Hero("Captain America",100));
+        heroes.add(new Hero("Vision",3));
+        heroes.add(new Hero("Iron Men",48));
+        heroes.add(new Hero("Scarlet Witch",29));
+        heroes.add(new Hero("Thor",1500));
+        heroes.add(new Hero("Hulk",49));
+        heroes.add(new Hero("Doktor Strange",42));
 
-        // TODO 5 : Keep only the half of the list
+        // It's Thor birthday, now he's 1501
+         Hero birthday = new Hero("Thor",1501); 
+         heroes.set(5,birthday);
 
-        // TODO 6 : Loop throught the list and display the name of the remaining heroes
+   
+        // Shuffle the heroes list
+         //before shuffle
+         System.out.println();
+         System.out.println("Before shuffle...");
+         for (Hero count : heroes) {
+            System.out.println (count.getName() + " " + count.getAge());
+           }
+
+           // Shuffle
+           Collections.shuffle(heroes);
+
+           System.out.println();
+           System.out.println("After the truffle shuffle...");
+           for (Hero count : heroes) {
+              System.out.println (count.getName() + " " + count.getAge());
+             }
+
+        // Keep only the half of the list
+         for (int number = 7; number >3; number--) {
+          heroes.remove(number);
+         }
+
+        // Loop throught the list and display the name of the remaining heroes
+
+        System.out.println();
+        System.out.println("Remaining participants...");        
+        for (Hero count : heroes) {
+         System.out.println (count.getName() + " " + count.getAge());
+        }
+
     }
 }
